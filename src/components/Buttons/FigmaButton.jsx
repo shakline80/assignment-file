57e6442f4f4dfe65c21f38141d54fb9554d831a0
shakline <shaklinemustak80@gmail.com>
2025-05-10 22:29:46 +0600

    Change figma icon path

--- src/components/Buttons/FigmaButton.jsx
+++ src/components/Buttons/FigmaButton.jsx
@@ -1,4 +1,4 @@
-import FimgaIcon from "../Icons/FigmaIcon";
+import FigmaIcon from '../Icons/FigmaIcon';
 
 const FigmaButon = ({ text = "View On Github", href = "#" }) => {
   return (
@@ -8,8 +8,8 @@ const FigmaButon = ({ text = "View On Github", href = "#" }) => {
       rel="noopener noreferrer"
       className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
     >
-        <FimgaIcon />
-        
+        <FigmaIcon />
+
       {text}
       
     </a>
--- src/components/Icons/Figmaicon.jsx
+++ src/components/Icons/Figmaicon.jsx
@@ -3,8 +3,8 @@
 import FimgaImg from '../../assets/figma.png';
 
 
-const FimgaIcon = () => (
+const FigmaIcon = () => (
    <img className='w-4 mr-2' src={FimgaImg} alt="figma icon" />
 );
 
-export default FimgaIcon;
+export default FigmaIcon;
