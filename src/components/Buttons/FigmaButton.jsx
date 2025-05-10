ec8ddf6165f5c6fb1fb4963b2418816812315ccb
shakline <shaklinemustak80@gmail.com>
2025-05-10 22:47:47 +0600

    New Changed

--- src/components/Buttons/FigmaButton.jsx
+++ src/components/Buttons/FigmaButton.jsx
@@ -13,7 +13,7 @@ const FigmaButon = ({ text = "View On Github", href = "#" }) => {
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+      className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-300 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
     >
         
         <FigmaIcon />
